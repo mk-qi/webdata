e70cd2303fe42bb622fc4300a55d8bbfaaa009aa
mk2011 <root@mkrss.com>
2015-01-28 11:36:21 +0800

    Update createhost.sh

--- createhost.sh
+++ createhost.sh
@@ -1,26 +1,24 @@
 #!/usr/bin/env bash
 # createhost.sh
 # used to create container for docker
+# 
 
-# DEFAULT VALUE globle
-#gen uuid for default hostname
+# DEFAULT VALUE 
+# --globle varibles explan
+# name; container name
+# img; docker images
+# cmd;  contianer run command /bin/bash --login 
+# ip/gw;   ipaddr for to conatainer 192.168.2.33/24 and gateway
+# ifname   bridge interface name
+
+# gen uuid for default hostname
 uuid=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 7 | head -n 1)
+# default container name
 name="${name:=sh-${uuid}}"
 
 # first we create an container without network
 # and store pids to pid
 # usage:  _c_fanli_conatainer $name "${options}" "${cmd}"  "${img}"
-#
-# globle var explan
-# name; container name
-# img; docker images
-# cmd;  contianer run command /bin/bash --login 
-
-
-# ip/gw;   ipaddr for to conatainer 192.168.2.33/24 and gateway
-# ifname   bridge interface name
-
-
 _c_fanli_conatainer(){
     host="$1";options="$2";
     cmd="$3";img="$4";volume=$6
@@ -68,60 +66,59 @@ _c_fanli_conatainer(){
 # call it with
 # _c_fanli_conatainer  $name  $cmd -d
 #
-# usage: _set_ip_forcontainer web1 192.168.2.22/24 192.168.2.1  docker0
+# usage: _set_ip_forcontainer name ip/mask gw  bridge-interface
+#    ex: _set_ip_forcontainer web1 192.168.2.22/24 192.168.2.1  docker0
 #
-_set_static_ip_forcontainer(){
-  name=$1;ip=$2;gw=$3;
-  ifname=$4;
-  
-  # get container pid
-  pid=$(sudo docker inspect -f '{{.State.Pid}}' $name 2> /dev/null);
-  [  $? != 0 ] && {
-    echo "No such image or container: ${name}";
-    exit 3;
-  }
-
-  # create a connecter between  host and container 
-  # but first we should make sure bridge interface
-  # exsit;
-  if [ -d /sys/class/net/$ifname/bridge ];then
-    if [ ! -d /var/run/netns ];then
-      sudo mkdir -p /var/run/netns
+_set_ip_forcontainer(){
+    name=$1;ip=$2;gw=$3;
+    ifname=$4;
+    
+    # get container pid
+    pid=$(sudo docker inspect -f '{{.State.Pid}}' $name 2> /dev/null);
+    [  $? != 0 ] && {
+        echo "No such image or container: ${name}";
+        exit 3;
+    }
+    # create a connecter between  host and container 
+    # but first we should make sure bridge interface
+    # exsit;
+    if [ -d /sys/class/net/$ifname/bridge ];then
+        if [ ! -d /var/run/netns ];then
+            sudo mkdir -p /var/run/netns
+        fi
+        if [ ! -f /var/run/netns/$pid ];then
+            sudo ln -s /proc/$pid/ns/net /var/run/netns/$pid
+        fi
     fi
-    if [ ! -f /var/run/netns/$pid ];then
-      sudo ln -s /proc/$pid/ns/net /var/run/netns/$pid
-    fi
-  fi
 
-  # set an ip for container
-  # we need to create a veth pair first
-  # lveth=>localhost veth
-  # gveth=>guesthost veth
-  lveth="lveth${pid}"
-  gveth="gveth${pid}"
+    # set an ip for container
+    # we need to create a veth pair first
+    # lveth=>localhost veth
+    # gveth=>guesthost veth
+    lveth="lveth${pid}"
+    gveth="gveth${pid}"
   
-  ip link add  $lveth type veth peer name $gveth
-  # add veth to bridge
-  ip link set $lveth master $ifname
-  ip link set $lveth up
-  # attache guestveth to container
-  ip link set $gveth netns $pid
+    ip link add  $lveth type veth peer name $gveth
+    # add veth to bridge
+    ip link set $lveth master $ifname
+    ip link set $lveth up
+    # attache guestveth to container
+    ip link set $gveth netns $pid
   
-  #ip namespace set ip and gw to container interface
-  ip netns exec $pid ip link set $gveth name eth0
-  ip netns exec $pid ip addr add $ip dev eth0
-  ip netns exec $pid ip link set eth0 up
-  # cut ip/netmask to ip
-  ipaddr=$(echo  $ip| cut -d/ -f1)
-  # send arp broadcast in container interface
-  ip netns exec $pid arping -c 1 -A -I eth0 $ipaddr
-  # set default gateway  for container
-  ip netns exec $pid ip route replace default via $gw
+    #ip namespace set ip and gw to container interface
+    ip netns exec $pid ip link set $gveth name eth0
+    ip netns exec $pid ip addr add $ip dev eth0
+    ip netns exec $pid ip link set eth0 up
+    # cut ip/netmask to ip
+    ipaddr=$(echo  $ip| cut -d/ -f1)
+    # send arp broadcast in container interface
+    ip netns exec $pid arping -c 1 -A -I eth0 $ipaddr
+    # set default gateway  for container
+    ip netns exec $pid ip route replace default via $gw
   
-  # Clean up dangling symlinks in /var/run/netns
-  find -L /var/run/netns -type l -delete
-  #[ -f /var/run/netns/$pid ] && rm -f /var/run/netns/$pid
-
+    # Clean up dangling symlinks in /var/run/netns
+    find -L /var/run/netns -type l -delete
+    #[ -f /var/run/netns/$pid ] && rm -f /var/run/netns/$pid
 }
 
 
@@ -165,13 +162,18 @@ while getopts "$optspec" optchar; do
     esac
 done
 
-
-if [  $dockeropt ];then
+# options parser
+if [ $dockeropt ];then
     options=$dockeropt
 elif [  $webdata ];then
     options=" --rm  -it"
+elif [ $ip ];then
+    options="--net=none -itd"
 else
     options=" --rm --net=none -it"
 fi
 _c_fanli_conatainer $name "${options}" "${cmd}"  "${img}"
 
+if [ $ip ];then
+    _set_ip_forcontainer $name $ip $gw  $ifname
+fi
